b8b4d5a7a4688a23bd3e25dcfd5aa64c725dd147
Bruce Bales <brucebales@Bruces-MacBook-Pro.local>
2025-02-01 20:25:19 -0800

    Still serve non-TLS HTTP when TLS is enabled

--- random-band/cmd/main.go
+++ random-band/cmd/main.go
@@ -20,10 +20,12 @@ func main() {
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/randomBand", randomBandHandler)
 	if os.Getenv("TLS_ENABLED") == "true" {
+		go func() {
+			log.Fatal(http.ListenAndServe(":80", nil))
+		}()
 		log.Fatal(http.ListenAndServeTLS(":443", os.Getenv("TLS_CERT_PATH"), os.Getenv("TLS_KEY_PATH"), nil))
 	} else {
 		log.Fatal(http.ListenAndServe(":80", nil))
-
 	}
 }
 
